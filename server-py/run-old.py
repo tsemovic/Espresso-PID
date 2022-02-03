a54b7ff325325e5f88fa2ebee0fea622afac4b65
Tully Semovic <semtb001@gmail.com>
2022-02-03 16:19:39 +1030

    socketio working on flask

--- server-py/run-old.py
+++ server-py/run-old.py
@@ -5,48 +5,19 @@ from datetime import datetime
 from flask_socketio import SocketIO, emit
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
-import RPi.GPIO as GPIO
-import signal
 from simple_pid import PID
 from flask_cors import CORS
 
 
 # Webserver setup
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
 socketio = SocketIO(app, logger=True, cors_allowed_origins="*")
 
-# PID setup
-P = 1
-I = 0.1
-D = 0.05
-pid = PID(P, I, D)
-pid.sample_time = 0.01
-pid.setpoint = 90
-
-# MAX3188 setup
-CLK = 4
-CS  = 3
-DO  = 2
-sensor = MAX31855.MAX31855(CLK, CS, DO)
-
-# SSR setup
-GPIO.setup(21, GPIO.OUT)
-GPIO.output(21, GPIO.HIGH)
-
-# Crtl-c handler (turns off SSR)
-def handler(signum, frame):
-    res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
-    if res == 'y':
-        GPIO.output(21, GPIO.LOW)
-        exit(1)
-signal.signal(signal.SIGINT, handler)
 
 # Webserver Routes
 @app.route('/')
 def home():  # At the same home function as before
-    return "<p>Hello</p>"
-
+    return "<p>HELLO</p>"
 
 @socketio.on('connect')
 def test_connect():

39c32c40cd25bff9301b3e465beeaacc73dcd6c9
Tully Semovic <semtb001@gmail.com>
2022-02-03 11:24:56 +1030

    quasar and socket working!!!

---
+++ client-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center">
+    <vs-button :active="active == 0" @click="active = 0">Active</vs-button>
+    <vs-button :active="active == 1" @click="active = 1">Default</vs-button>
+    <vs-button disabled>Disabled</vs-button>
+  </div>
+</template>
+
+
+<script>
+import SocketioService from './services/socketio.service.js';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created() {
+    SocketioService.setupSocketConnection();
+  },
+  beforeUnmount() {
+    SocketioService.disconnect();
+  }
+}
+</script>

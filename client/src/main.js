59718ad1cad291615cda2e7ffefacb01d8efceaf
Tully Semovic <semtb001@gmail.com>
2022-02-20 16:48:06 +1030

    *

--- client/src/main.js
+++ client/src/main.js
@@ -3,18 +3,18 @@ import App from "./App.vue";
 import { Quasar, Notify } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
 
-// import axios from "axios";
+import axios from "axios";
 
 async function getSettings() {
-  // let res = await axios.get("/settings");
-  // return await res.data;
-  let json = {
-    "VUE_TITLE" : "GAGGIA CONTROLLER",
-    "VUE_COLOUR" : {"primary" : "#D0D9B0", "secondary" : "#403324", "accent" : "#736049", "info" : "#736049"},
-    "FLASK_IP" : "192.168.1.21",
-    "FLASK_PORT" : 80
-}
-  return json
+  let res = await axios.get("/settings");
+  return await res.data;
+//   let json = {
+//     "VUE_TITLE" : "GAGGIA CONTROLLER",
+//     "VUE_COLOUR" : {"primary" : "#D0D9B0", "secondary" : "#403324", "accent" : "#736049", "info" : "#736049"},
+//     "FLASK_IP" : "192.168.1.21",
+//     "FLASK_PORT" : 80
+// }
+//   return json
 }
 
 getSettings().then((res) => {
